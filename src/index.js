d5bf82022e4ed7e82b271638b4981161ebe38cae
BoRrrr <robialwan8@gmail.com>
2025-05-26 23:44:41 +0700

    perbaikan endpoint

--- src/index.js
+++ src/index.js
@@ -17,6 +17,11 @@ app.use(
     })
 );
 app.use(express.json());
+app.use((req, res, next) => {
+    console.log(`Request masuk: ${req.method} ${req.url}`);
+    next();
+});
+
 const PORT = process.env.PORT;
 
 //1
